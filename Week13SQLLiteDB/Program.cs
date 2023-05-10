225c6c629580164ba0d2fba2fc68f3e41e57cc4e
Krete <krete121@gmail.com>
2023-05-10 16:04:21 +0300

    add week13 code

---
+++ Week13SQLLiteDB/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SQLite;
+SQLiteConnection connection = new SQLiteConnection("Data Source=mydb.db; Version = 3; New = True; Compress = True;");
+try
+{
+    connection.Open();
+    Console.WriteLine("DB found.");
+}
+catch
+{
+    Console.WriteLine("DB not found.");
+}
